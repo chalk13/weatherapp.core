2ed56f366e942344efa34f8caab27c87b12420de
Mazzart <am.magicsmile@gmail.com>
2018-12-13 22:08:47 +0200

    Add function to get the page and collect the needed information.

--- providers.py
+++ providers.py
@@ -10,6 +10,7 @@ import os
 import shutil
 import time
 from pathlib import Path
+from urllib.request import urlopen, Request
 
 from bs4 import BeautifulSoup
 
@@ -120,3 +121,83 @@ class AccuWeatherProvider:
                 life_time = time.time() - (path / file).stat().st_mtime
                 if life_time > config.DAY_IN_SECONDS:
                     os.remove(path / file)
+
+    def get_page_from_server(self, page_url: str, refresh: bool = False) -> str:
+        """Return information about the page in the string format"""
+
+        cache = self.get_cache(page_url)
+        if cache and not refresh:
+            page = cache
+        else:
+            request = Request(page_url, headers=self.get_request_headers())
+            page = urlopen(request).read()
+            self.save_cache(page_url, page)
+
+        return page.decode('utf-8')
+
+    def get_locations_accu(self, locations_url: str, refresh: bool = False) -> list:
+        """Return a list of locations and related urls"""
+
+        locations_page = self.get_page_from_server(locations_url, refresh=refresh)
+        soup = BeautifulSoup(locations_page, 'html.parser')
+
+        locations = []
+        for location in soup.find_all('li', class_='drilldown cl'):
+            url = location.find('a').attrs['href']
+            location = location.find('em').text
+            locations.append((location, url))
+        return locations
+
+    def configuration(self, command: str, refresh: bool = False):
+        """Set the location for which to display the weather"""
+
+        if command == 'accu':
+            locations = self.get_locations_accu(BROWSE_LOCATIONS[command],
+                                                refresh=refresh)
+        elif command == 'rp5':
+            locations = self.get_locations_rp5(BROWSE_LOCATIONS[command],
+                                               refresh=refresh)
+
+        while locations:
+            for index, location in enumerate(locations):
+                print(f'{index + 1}) {location[0]}')
+            selected_index = int(input('Please select location: '))
+            location = locations[selected_index - 1]
+            if command == 'accu':
+                locations = self.get_locations_accu(location[1], refresh=refresh)
+            elif command == 'rp5':
+                locations = self.get_locations_rp5(location[1], refresh=refresh)
+
+        self.save_configuration(command, *location)
+
+    def get_weather_accu(self, page: str, refresh: bool = False) -> dict:
+        """Return information collected from AccuWeather"""
+
+        weather_page = BeautifulSoup(page, 'html.parser')
+        current_day_selection = weather_page.find('li', {'class': ['day current first cl',
+                                                                   'night current first cl']})
+
+        weather_info = {}
+        if current_day_selection:
+            current_day_url = current_day_selection.find('a').attrs['href']
+            if current_day_url:
+                current_day_page = self.get_page_from_server(current_day_url,
+                                                             refresh=refresh)
+                if current_day_page:
+                    current_day = BeautifulSoup(current_day_page, 'html.parser')
+                    weather_details = current_day.find('div', attrs={'id': 'detail-now'})
+                    temp = weather_details.find('span', class_='large-temp')
+                    if temp:
+                        weather_info['Temperature'] = temp.text
+                    condition = weather_details.find('span', class_='cond')
+                    if condition:
+                        weather_info['Condition'] = condition.text
+
+                    feel_temp = weather_details.find('span', class_='small-temp')
+                    if feel_temp:
+                        weather_info['RealFeel'] = feel_temp.text
+                    wind_info = weather_details.find_all('li', class_='wind')
+                    if wind_info:
+                        weather_info['Wind'] = ' '.join(map(lambda t: t.text.strip(), wind_info))
+
+        return weather_info
