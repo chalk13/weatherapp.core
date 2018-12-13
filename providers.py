b5bb67df54dc6fe2c6157f358221e7a811946383
Mazzart <am.magicsmile@gmail.com>
2018-12-13 21:27:01 +0200

    Add function get_configuration_file and extend docstring.

--- providers.py
+++ providers.py
@@ -1,4 +1,8 @@
+""""""
+
+import configparser
 import config
+from pathlib import Path
 
 
 class AccuWeatherProvider:
@@ -8,20 +12,30 @@ class AccuWeatherProvider:
 
     def __init__(self):
         self.name = config.ACCU_PROVIDER_NAME
-        self.location = ''
-        self.url = ''
 
-    def get_configuration(command: str) -> tuple:
+        location, url = self.get_configuration()
+        self.location = location
+        self.url = url
+
+    def get_configuration(self, command: str) -> tuple:
         """Returns name of the city and related url"""
 
-        name = DEFAULT_NAME
-        url = DEFAULT_URL[command]
+        name = config.DEFAULT_NAME
+        url = config.DEFAULT_URL[command]
 
         parser = configparser.ConfigParser()
-        parser.read(get_configuration_file())
+        parser.read(self.get_configuration_file())
 
         if command in parser.sections():
             config = parser[command]
             name, url = config['name'], config['url']
 
         return name, url
+
+    def get_configuration_file(self):
+        """Path to the CONFIG_FILE.
+
+        Returns path to configuration file in your home directory.
+        """
+
+        return Path.home() / config.CONFIG_FILE
