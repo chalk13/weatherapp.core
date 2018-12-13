f3a64ae7afafa095ffac23c01ce7afe70df1905e
Mazzart <am.magicsmile@gmail.com>
2018-12-13 21:46:07 +0200

    Add new constant.

--- config.py
+++ config.py
@@ -2,11 +2,14 @@
 
 ACCU_PROVIDER_NAME = 'accu'
 RP5_PROVIDER_NAME = 'rp5'
+FAKE_MOZILLA_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6)'
+
 DEFAULT_NAME = 'Kyiv'
 DEFAULT_URL = {'accu': 'https://www.accuweather.com/en/ua/kyiv/324505/weather-forecast/324505',
                'rp5': 'http://rp5.ua/Weather_in_Kiev,_Kyiv'}
 BROWSE_LOCATIONS = {'accu': 'https://www.accuweather.com/en/browse-locations',
                     'rp5': 'http://rp5.ua/Weather_in_the_world'}
+
 CONFIG_FILE = 'weatherapp.ini'
 CACHE_DIR = '.weatherappcache'
 CACHE_TIME = 900
