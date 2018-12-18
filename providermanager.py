61acc659526e518e0663d22eaad1261744cf69a9
Mazzart <am.magicsmile@gmail.com>
2018-12-18 05:52:10 +0200

    Added add method

--- providermanager.py
+++ providermanager.py
@@ -4,8 +4,12 @@ class ProviderManager:
     def __init__(self):
         self._providers = {}
 
+    def add(self, name, provider):
+        """Add new provider by name"""
+
+        self._providers[name] = provider
+
     def get(self, name):
         """Get provider by name"""
 
         return self._providers.get([name], None)
-    
