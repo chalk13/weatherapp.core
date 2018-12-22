807ca3fe254e545fcf4a8360556c79e5d866d046
Mazzart <am.magicsmile@gmail.com>
2018-12-22 06:06:28 +0200

    Added config_location variable

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 """Constants for the weather application project."""
 
-ACCU_PROVIDER_NAME = 'accu'
-RP5_PROVIDER_NAME = 'rp5'
+ACCU_PROVIDER_NAME = 'accu'  # provider id
+RP5_PROVIDER_NAME = 'rp5'  # provider id
 
-ACCU_PROVIDER_TITLE = 'AccuWeather'
-RP5_PROVIDER_TITLE = 'RP5'
+ACCU_PROVIDER_TITLE = 'AccuWeather'  # provider title
+RP5_PROVIDER_TITLE = 'RP5'  # provider title
 
 FAKE_MOZILLA_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6)'
 
@@ -17,6 +17,7 @@ BROWSE_LOCATIONS = {'accu': 'https://www.accuweather.com/en/browse-locations',
                     'rp5': 'http://rp5.ua/Weather_in_the_world'}
 
 CONFIG_FILE = 'weatherapp.ini'
+CONFIG_LOCATION = 'Location'
 CACHE_DIR = '.weatherappcache'
 CACHE_TIME = 900
 DAY_IN_SECONDS = 86400
