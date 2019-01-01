0340443c4673d11261783d93ce5147dbf9210318
Mazzart <am.magicsmile@gmail.com>
2019-01-01 21:57:03 +0200

    Added singleton decorator

--- decorators.py
+++ decorators.py
@@ -110,3 +110,15 @@ def fibonacci(n):
 
 
 print(f'The n-th Fibonacci number: {fibonacci(42)}')
+
+
+def singleton(cls):
+    """Singleton decorator"""
+    instances = {}
+
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return wrapper
