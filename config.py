7347cfbbde194aba359af072bd3b994033352cd6
Mazzart <am.magicsmile@gmail.com>
2019-01-04 21:52:45 +0200

    Added comments

--- config.py
+++ config.py
@@ -1,20 +1,24 @@
 """Constants for the weather application project."""
 
-ACCU_PROVIDER_NAME = 'accu'  # provider id
-RP5_PROVIDER_NAME = 'rp5'  # provider id
-
-ACCU_PROVIDER_TITLE = 'AccuWeather'  # provider title
-RP5_PROVIDER_TITLE = 'RP5'  # provider title
-
 FAKE_MOZILLA_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6)'
 
-DEFAULT_NAME = 'Kyiv'
+# AccuWeather provider related configuration
+ACCU_PROVIDER_NAME = 'accu'  # provider id
+ACCU_PROVIDER_TITLE = 'AccuWeather'  # provider title
 DEFAULT_URL_ACCU = 'https://www.accuweather.com/en/ua/kyiv/324505/weather-forecast/324505'
+
+# RP5 provider related configuration
+RP5_PROVIDER_NAME = 'rp5'  # provider id
+RP5_PROVIDER_TITLE = 'RP5'  # provider title
 DEFAULT_URL_RP5 = 'http://rp5.ua/Weather_in_Kiev,_Kyiv'
+
+DEFAULT_NAME = 'Kyiv'  # default location, common for each provider
 BROWSE_LOCATIONS = {'accu': 'https://www.accuweather.com/en/browse-locations',
                     'rp5': 'http://rp5.ua/Weather_in_the_world'}
 
-CONFIG_FILE = 'weatherapp.ini'
-CACHE_DIR = '.weatherappcache'
-CACHE_TIME = 900
-DAY_IN_SECONDS = 86400
+CONFIG_FILE = 'weatherapp.ini'  # configuration file name
+
+# Cache settings
+CACHE_DIR = '.weatherappcache'  # cache directory name
+CACHE_TIME = 900  # how long cache files are valid (in seconds)
+DAY_IN_SECONDS = 86400  # time during which the cache is not removed
