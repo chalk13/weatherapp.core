c0ec86e50f5fdb3f4b699515f6afde6828f52596
Mazzart <am.magicsmile@gmail.com>
2019-01-05 23:18:56 +0200

    Task done (changes in abstract.py)

--- weatherapp.py
+++ weatherapp.py
@@ -1,6 +1,5 @@
 """Weather application TODO list.
 """
 
-# TODO: weatherapp.ini must contain information from both sites
 # TODO: add a command that shows the weather for tomorrow
 # TODO: add SINOPTIK weather site
