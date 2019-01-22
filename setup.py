bc32cbf86402a59a0de8868c00261870d5eb0e75
Mazzart <am.magicsmile@gmail.com>
2019-01-22 06:10:17 +0200

    Added install script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="weatherapp.core",
+    version="0.1.0",  # MAJOR.MINOR.PATCH
+    author="Artem Mazur",
+    description="A simple weather aggregator",
+    long_description="",
+    packages=find_namespace_packages(),
+    entry_points={
+        'console_scripts': 'wfapp=weatherapp.core.app:main'
+    },
+    install_requires=[
+        'bs4',
+        'requests',
+        'colorlog',
+    ]
+)
