cafeb2d808ac9174c7718f4a0e2cf9ddf82b15ec
Mazzart <am.magicsmile@gmail.com>
2019-01-22 20:24:23 +0200

    Delete unused files after reorganization

--- weatherapp.py
+++
@@ -1,7 +0,0 @@
-"""Weather application.
-"""
-
-# TODO: add a command that shows the weather for tomorrow
-# TODO: add SINOPTIK weather site
-# TODO: fix start = first_sentence.rfind(',') + 2
-# TODO: write log info to diff files according to log level (DEBUG, ...)
