61f3cdd3b8fc6d79e62903c002349df5ac07983b
Mazzart <am.magicsmile@gmail.com>
2019-01-28 22:25:19 +0200

    Added table formatter

---
+++ weatherapp/core/formatters/table.py
@@ -0,0 +1,21 @@
+import prettytable
+
+from weatherapp.core.abstract import Formatter
+from typing import Union
+
+
+class TableFormatter(Formatter):
+    """Table formatter for application output."""
+
+    def emit(self, column_names: list, data: Union[list, tuple]):
+        """Format and print data from the iterable source."""
+
+        pt = prettytable.PrettyTable()
+
+        for column, values in zip(column_names, (data.keys(), data.values())):
+            if any(values):
+                pt.add_column(column, list(values))
+
+        pt.align = 'l'
+        pt.padding_width = 1
+        return pt.get_string()
