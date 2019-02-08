bcef2d7e098f0aec2b613ce65d528d7f5be1f741
Mazzart <am.magicsmile@gmail.com>
2019-02-08 07:12:14 +0200

    Refactor code and add tests for _load_providers and __len__ methods

--- weatherapp/core/tests/unit/test_providermanager.py
+++ weatherapp/core/tests/unit/test_providermanager.py
@@ -13,11 +13,18 @@ class ProviderManagerTestCase(unittest.TestCase):
     def setUp(self):
         """Contain set up info for every single test."""
         self.provider_manager = ProviderManager()
+        self.provider_manager.add('example', ExampleCommand)
+
+    def test_load_providers(self):
+        """Test _load_providers method for provider manager."""
+
+        message = 'An error occurs during the _load_providers method.'
+        self.assertTrue('accu' in self.provider_manager._providers, msg=message)
+        self.assertTrue('rp5' in self.provider_manager._providers, msg=message)
+        self.assertTrue('sinoptik' in self.provider_manager._providers, msg=message)
 
     def test_add(self):
         """Test add method for provider manager."""
-
-        self.provider_manager.add('example', ExampleCommand)
 
         self.assertTrue('example' in self.provider_manager._providers,
                         msg="Command 'example' is missing in provider manager.")
@@ -25,18 +32,19 @@ class ProviderManagerTestCase(unittest.TestCase):
     def test_get(self):
         """Test get method for provider manager."""
 
-        self.provider_manager.add('example', ExampleCommand)
-
         self.assertEqual(self.provider_manager.get('example'), ExampleCommand)
         self.assertIsNone(self.provider_manager.get('bar'), ExampleCommand)
 
     def test_contains(self):
         """Test if '__contains__' method is working."""
 
-        self.provider_manager.add('example', ExampleCommand)
-
         self.assertTrue('example' in self.provider_manager)
         self.assertFalse('bar' in self.provider_manager)
+
+    def test_len(self):
+        """Test if '__len__' method is working."""
+
+        self.assertEqual(4, len(self.provider_manager._providers))
 
 
 if __name__ == '__main__':
