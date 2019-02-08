ae542a7a6f0048ae791fbecf08a2247d13c47394
Mazzart <am.magicsmile@gmail.com>
2019-02-08 07:10:31 +0200

    Refactor code and add test for _load_commands method

--- weatherapp/core/tests/unit/test_commandmanager.py
+++ weatherapp/core/tests/unit/test_commandmanager.py
@@ -13,27 +13,29 @@ class CommandManagerTestCase(unittest.TestCase):
     def setUp(self):
         """Contain set up info for every single test."""
         self.command_manager = CommandManager()
+        self.command_manager.add('example', ExampleCommand)
 
     def test_add(self):
         """Test add method for command manager."""
 
-        self.command_manager.add('example', ExampleCommand)
-
         self.assertTrue('example' in self.command_manager._commands,
                         msg="Command 'example' is missing in command manager.")
 
+    def test_load_commands(self):
+        """Test _load_commands method for command manager."""
+
+        message = 'An error occurs during the _load_commands method.'
+        self.assertTrue('config' in self.command_manager._commands, msg=message)
+        self.assertTrue('providers' in self.command_manager._commands, msg=message)
+
     def test_get(self):
         """Test get method for command manager."""
-
-        self.command_manager.add('example', ExampleCommand)
 
         self.assertEqual(self.command_manager.get('example'), ExampleCommand)
         self.assertIsNone(self.command_manager.get('bar'), ExampleCommand)
 
     def test_contains(self):
         """Test if '__contains__' method is working."""
-
-        self.command_manager.add('example', ExampleCommand)
 
         self.assertTrue('example' in self.command_manager)
         self.assertFalse('bar' in self.command_manager)
