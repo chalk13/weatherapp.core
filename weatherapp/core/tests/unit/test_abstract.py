86c9f57bf4956061071a0c69b6c818d4b2629b03
Mazzart <am.magicsmile@gmail.com>
2019-02-09 13:00:08 +0200

    Added unitest for abstract classes

---
+++ weatherapp/core/tests/unit/test_abstract.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import patch
+
+from weatherapp.core.abstract import Formatter, Manager
+
+
+class FormatterTestCase(unittest.TestCase):
+    """Unit test case for formatter abstract class"""
+
+    @patch.multiple(Formatter, __abstractmethods__=set())
+    def test(self):
+        self.instance = Formatter()
+
+
+class ManagerTestCase(unittest.TestCase):
+    """Unit test case for manager abstract class"""
+
+    @patch.multiple(Manager, __abstractmethods__=set())
+    def test(self):
+        self.instance = Manager()
+
+
+if __name__ == '__main__':
+    unittest.main()
