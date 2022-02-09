867b825faaf041293646564eb8418bb7acc3c353
Mazzart <am.magicsmile@gmail.com>
2022-02-09 23:02:45 +0200

    fix information extraction for accu provider, lines reduction

--- weatherapp/core/providers.py
+++ weatherapp/core/providers.py
@@ -36,10 +36,12 @@ class AccuWeatherProvider(WeatherProvider):
         soup = BeautifulSoup(locations_page, 'html.parser')
 
         locations = []
-        for location in soup.find_all('li', class_='drilldown cl'):
-            url = location.find('a').attrs['href']
-            location = location.find('em').text
-            locations.append((location, url))
+        places = soup.find('div', class_='result-container')
+        if places:
+            for place in places.find_all('a'):
+                path = place.attrs['href']
+                location = place.text
+                locations.append((location, f'https://www.accuweather.com/{path}'))
         return locations
 
     def configuration(self, command: str, refresh: bool = False):
@@ -77,7 +79,7 @@ class AccuWeatherProvider(WeatherProvider):
         """Return information collected from AccuWeather."""
 
         weather_page = BeautifulSoup(page, 'html.parser')
-        current_day_selection = weather_page.find_all('a', class_='current')
+        current_day_selection = weather_page.find_all('a', class_='cur-con-weather-card')
 
         weather_info = {}
         if current_day_selection:
@@ -88,14 +90,14 @@ class AccuWeatherProvider(WeatherProvider):
                                                              refresh=refresh)
                 if current_day_page:
                     current_day = BeautifulSoup(current_day_page, 'html.parser')
-                    temp = current_day.find('div', class_='temperatures')
-                    curr_temp = temp.find('p', class_='value')
+                    temp = current_day.find('div', class_='card-content')
+                    curr_temp = temp.find('div', class_='display-temp')
                     if curr_temp:
                         weather_info['Temperature'] = curr_temp.text.strip()
                     condition = current_day.find('div', class_='phrase')
                     if condition:
                         weather_info['Condition'] = condition.text
-                    feel_temp = temp.find('p', class_='realFeel top')
+                    feel_temp = temp.find('div', class_='current-weather-extra')
                     if feel_temp:
                         curr_feel_temp = ''.join(i for i in feel_temp.text if i.isdigit())
                         if '-' in curr_feel_temp:
@@ -137,23 +139,20 @@ class Rp5WeatherProvider(WeatherProvider):
             if not places:
                 places = soup.find_all('div', class_='city_link')
                 for place in places:
-                    url = place.find('a').attrs['href']
-                    url = f'http://rp5.ua/{url}'
+                    path = place.find('a').attrs['href']
                     location = place.text
-                    locations.append((location, url))
+                    locations.append((location, f'https://rp5.ua/{path}'))
                 return locations
             for place in places:
-                url = place.attrs['href']
-                url = f'http://rp5.ua/{url}'
+                path = place.attrs['href']
                 location = place.text
-                locations.append((location, url))
+                locations.append((location, f'https://rp5.ua/{path}'))
         else:
             for location in places:
-                url = location.find('b')
-                url = url.find('a').attrs['href']
-                url = f'http://rp5.ua{url}'
+                path = location.find('b')
+                path = path.find('a').attrs['href']
                 location = location.find('b').text[:-1]
-                locations.append((location, url))
+                locations.append((location, f'https://rp5.ua{path}'))
 
         return locations
 
@@ -248,9 +247,8 @@ class SinoptikWeatherProvider(WeatherProvider):
         if places:
             for place in places.find_all('a'):
                 location = place.text
-                url = place.attrs['href']
-                url = f'https:{url}'
-                locations.append((location, url))
+                link = place.attrs['href']
+                locations.append((location, f'https:{link}'))
 
         return locations
 
